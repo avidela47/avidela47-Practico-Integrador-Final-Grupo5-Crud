112fcd9e3f4fd6c346c87ae76109150d7adc0735
Ariel Videla <ariel_videla@hotmail.com>
2024-06-27 15:38:34 -0300

    first commit

--- Server/index.js
+++ Server/index.js
@@ -3,38 +3,39 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import colors from "colors";
 import path from "path";
-import { fileURLToPath } from "url"; // Importa fileURLToPath
-import { adminRouter } from "./Routes/AdminRoute.js"; // Ajusta la ruta si es necesario
+import { fileURLToPath } from "url";
+import { adminRouter } from "./Routes/AdminRoute.js";
 import { employeeRouter } from "./Routes/EmployeeRoute.js";
 import Jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 
-// Definir __dirname manualmente
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Setting
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8080;
-const publicRoot = path.join(__dirname, "uploads");
+
 app.use(cookieParser());
 
-// Middlewares
 app.use(
   cors({
-    origin: ["https://vinyl-record.vercel.app"],
+    origin: "https://vinyl-record.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static(publicRoot)); // Configurar la ruta estática correctamente
 
-// Routes
+const publicRoot = path.join(__dirname, "uploads");
+app.use("/uploads", express.static(publicRoot));
+
+// Verificar rutas y middlewares
 app.use("/auth", adminRouter);
 app.use("/employee", employeeRouter);
+
 const verifyUser = (req, res, next) => {
   const token = req.cookies.token;
   if (token) {
@@ -48,15 +49,14 @@ const verifyUser = (req, res, next) => {
     return res.json({ Status: false, Error: "No Autenticado" });
   }
 };
+
+// Asegúrate de que esta ruta está después del middleware de verificación
 app.get("/verify", verifyUser, (req, res) => {
   return res.json({ Status: true, role: req.role, id: req.id });
 });
 
-// Server
 const httpServer = app.listen(PORT, async () => {
-  console.log(
-    `Server corriendo en modo ${process.env.DEV_MODE} en puerto ${PORT}`
-      .bgMagenta.white
-  );
+  console.log(`Server corriendo en modo ${process.env.DEV_MODE} en puerto ${PORT}`.bgMagenta.white);
 });
+
 httpServer.on("error", (error) => console.log(`Error: ${error}`.bgRed.white));
